ddafdc0ec60e186ca6778440c5baf151321c792e
Piyush Kumar <piyush2004.dev@gmail.com>
2026-05-15 06:58:13 +0530

    chore: add http code

---
+++ backend/src/core/constants/http.ts
@@ -0,0 +1,18 @@
+export const HttpStatus = {
+  // 2xx
+  OK: 200,
+  CREATED: 201,
+  NO_CONTENT: 204,
+
+  // 4xx
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  CONFLICT: 409,
+  UNPROCESSABLE_ENTITY: 422,
+  TOO_MANY_REQUESTS: 429,
+
+  // 5xx
+  INTERNAL_SERVER_ERROR: 500,
+} as const;
