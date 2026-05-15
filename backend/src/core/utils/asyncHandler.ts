e83ef9779ab335ef2a1292aab60a4eb718e59daa
Piyush Kumar <piyush2004.dev@gmail.com>
2026-05-15 06:59:53 +0530

    faet: create asyncHandler to get rid try catch

---
+++ backend/src/core/utils/asyncHandler.ts
@@ -0,0 +1,17 @@
+import type { Request, Response, NextFunction, RequestHandler } from 'express';
+
+type AsyncRequestHandler = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => Promise<void>;
+
+export function asyncHandler(fn: AsyncRequestHandler): RequestHandler {
+  return (req, res, next) => {
+    try {
+      Promise.resolve(fn(req, res, next)).catch(next);
+    } catch (err) {
+      next(err);
+    }
+  };
+}
